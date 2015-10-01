f40e914540e2373942ba3eaa5a0a7e3f12b2a4b1
Iwan Petrov <ivansharp@yandex.ru>
2015-10-01 13:03:56 +0600

    Create Tier3.java

---
+++ src/com.myowndev.invasion.entity.enemies/Tier3.java
@@ -0,0 +1,119 @@
+package com.myowndev.invasion.entity.enemies;
+
+import com.myowndev.invasion.entity.Enemy;
+
+/**
+ * Created by Iwan on 22.09.2015.
+ */
+public class Tier3 extends Enemy {
+    public Tier3(int start_x, int start_y) {
+        super(start_x, start_y);
+        speed = 2;
+        scoreGain = 2;
+        scoreLoss = 2;
+        hp = 4;
+        silverGain = 2;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

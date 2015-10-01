ae1f9be71afd51a9ebf38fa39e429f4887da05aa
Iwan Petrov <ivansharp@yandex.ru>
2015-10-01 13:03:11 +0600

    Create Tier1.java

---
+++ src/com.myowndev.invasion.entity.enemies/Tier1.java
@@ -0,0 +1,89 @@
+package com.myowndev.invasion.entity.enemies;
+
+import com.myowndev.invasion.entity.Enemy;
+
+/**
+ * Created by Iwan on 22.09.2015.
+ */
+public class Tier1 extends Enemy {
+    public Tier1(int start_x, int start_y) {
+        super(start_x, start_y);
+        speed = 2;
+        scoreGain = 1;
+        scoreLoss = 1;
+        hp = 1;
+        silverGain = 1;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

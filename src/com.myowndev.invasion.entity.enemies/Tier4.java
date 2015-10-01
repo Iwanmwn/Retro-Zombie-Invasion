3b7e06daff5ada2086bc3dd7692337852d9a14d4
Iwan Petrov <ivansharp@yandex.ru>
2015-10-01 13:04:10 +0600

    Create Tier4.java

---
+++ src/com.myowndev.invasion.entity.enemies/Tier4.java
@@ -0,0 +1,104 @@
+package com.myowndev.invasion.entity.enemies;
+
+import com.myowndev.invasion.entity.Enemy;
+
+/**
+ * Created by Iwan on 22.09.2015.
+ */
+public class Tier4 extends Enemy {
+    public Tier4(int start_x, int start_y) {
+        super(start_x, start_y);
+        speed = 6;
+        scoreGain = 2;
+        scoreLoss = 4;
+        hp = 2;
+        silverGain = 2;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
